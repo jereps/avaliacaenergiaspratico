aeb027215c5e1c2f8006f2227140507846f2f3ec
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-07-13 21:54:36 -0300

    exercicio 2

---
+++ Pacotes/src/busca.java
@@ -0,0 +1,11 @@
+
+public class busca {
+
+	public static void main(String args[]) {
+		pacote pacote1 = new pacote("pacote1", 2, "ceara", "azul", 20, 200);
+		pacote pacote2 = new pacote("pacote2", 3, "amazonas", "azul", 20, 300);
+		
+		
+
+	}
+}
---
+++ Pacotes/src/pacote.java
@@ -0,0 +1,24 @@
+
+public class pacote {
+	
+	String 	Nome;
+	int dias;
+	String regiao;
+	String ciaaerea;
+	int horasvoo;
+	float preco;
+	
+	public pacote(String nome, int dias, String regiao, String ciaaerea, int horasvoo, float preco) {
+		super();
+		Nome = nome;
+		this.dias = dias;
+		this.regiao = regiao;
+		this.ciaaerea = ciaaerea;
+		this.horasvoo = horasvoo;
+		this.preco = preco;
+	}
+	
+	
+	
+
+}
